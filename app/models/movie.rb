a59b9cb50b6d99bf8ac722da23777763d0508924
Celia Wilford <celiawilford@gmail.com>
2023-03-08 15:09:25 +0100

    dependency fixed for movie & movie_lists

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :list_movies
+  has_many :list_movies, dependent: :destroy
   has_many :lists, through: :list_movies
   has_many :movie_reviews, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
 end
+
