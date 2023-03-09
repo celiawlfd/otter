97147a6ddf392afba34065a4ee156471dc9adfea
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-03-09 20:00:29 +0100

    en cours de creation des seeds pour les podcasts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_115110) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_184932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,6 +110,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_115110) do
     t.integer "duration"
     t.string "photo_url"
     t.string "director", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "podcasts", force: :cascade do |t|
+    t.string "title"
+    t.string "publisher"
+    t.string "image_url"
+    t.float "lenght"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,75 +9,102 @@
 require "json"
 require "open-uri" # GEM to parse data from web
 
-url_movies = "https://tmdb.lewagon.com/movie/top_rated"
-movies = JSON.parse(URI.open(url_movies).read)
+# url_movies = "https://tmdb.lewagon.com/movie/top_rated"
+# movies = JSON.parse(URI.open(url_movies).read)
 
-Movie.destroy_all
-puts "creating movies"
+# Movie.destroy_all
+# puts "creating movies"
 
-movies["results"].each do |movie|
-  movie = Movie.new(
-    title: movie['original_title'],
-    description: movie['overview'],
-    photo_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
-    year: movie['release_date'],
-    duration: "",
-    director: ""
-  )
-  movie.save!
+# movies["results"].each do |movie|
+#   movie = Movie.new(
+#     title: movie['original_title'],
+#     description: movie['overview'],
+#     photo_url: "https://image.tmdb.org/t/p/original/#{movie['poster_path']}",
+#     year: movie['release_date'],
+#     duration: "",
+#     director: ""
+#   )
+#   movie.save!
 
-  puts "creating movie #{movie.id}, #{movie.title}"
-end
+#   puts "creating movie #{movie.id}, #{movie.title}"
+# end
 
-Book.destroy_all
-puts "Creating Books"
+# Book.destroy_all
+# puts "Creating Books"
 
-url_books = "https://www.googleapis.com/books/v1/volumes?q=war"
-books = JSON.parse(URI.open(url_books).read)
+# url_books = "https://www.googleapis.com/books/v1/volumes?q=war"
+# books = JSON.parse(URI.open(url_books).read)
 
-books["items"].each do |book|
-  book = Book.new(
-    title: book["volumeInfo"]['title'],
-    author: book["volumeInfo"]['authors'],
-    description: book["volumeInfo"]['description'],
-    photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
-    year: book["volumeInfo"]['publishedDate'],
-    pages_number: book["volumeInfo"]['pageCount']
-  )
-  book.save
-  puts "creating book #{book.id}, #{book.title}"
-end
+# books["items"].each do |book|
+#   book = Book.new(
+#     title: book["volumeInfo"]['title'],
+#     author: book["volumeInfo"]['authors'],
+#     description: book["volumeInfo"]['description'],
+#     photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
+#     year: book["volumeInfo"]['publishedDate'],
+#     pages_number: book["volumeInfo"]['pageCount']
+#   )
+#   book.save
+#   puts "creating book #{book.id}, #{book.title}"
+# end
 
 
-url_books = "https://www.googleapis.com/books/v1/volumes?q=cats"
-books = JSON.parse(URI.open(url_books).read)
+# url_books = "https://www.googleapis.com/books/v1/volumes?q=cats"
+# books = JSON.parse(URI.open(url_books).read)
 
-books["items"].each do |book|
-  book = Book.new(
-    title: book["volumeInfo"]['title'],
-    author: book["volumeInfo"]['authors'],
-    description: book["volumeInfo"]['description'],
-    photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
-    year: book["volumeInfo"]['publishedDate'],
-    pages_number: book["volumeInfo"]['pageCount']
-  )
-  book.save
-  puts "creating book #{book.id}, #{book.title}"
-end
+# books["items"].each do |book|
+#   book = Book.new(
+#     title: book["volumeInfo"]['title'],
+#     author: book["volumeInfo"]['authors'],
+#     description: book["volumeInfo"]['description'],
+#     photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
+#     year: book["volumeInfo"]['publishedDate'],
+#     pages_number: book["volumeInfo"]['pageCount']
+#   )
+#   book.save
+#   puts "creating book #{book.id}, #{book.title}"
+# end
 
-  url_books = "https://www.googleapis.com/books/v1/volumes?q=seals"
-books = JSON.parse(URI.open(url_books).read)
+#   url_books = "https://www.googleapis.com/books/v1/volumes?q=seals"
+# books = JSON.parse(URI.open(url_books).read)
 
-books["items"].take(3).each do |book|
-  book = Book.new(
-    title: book["volumeInfo"]['title'],
-    author: book["volumeInfo"]['authors'],
-    description: book["volumeInfo"]['description'],
-    photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
-    year: book["volumeInfo"]['publishedDate'],
-    pages_number: book["volumeInfo"]['pageCount']
-  )
-  book.save
-  puts "creating book #{book.id}, #{book.title}"
-end
-puts "done"
+# books["items"].take(3).each do |book|
+#   book = Book.new(
+#     title: book["volumeInfo"]['title'],
+#     author: book["volumeInfo"]['authors'],
+#     description: book["volumeInfo"]['description'],
+#     photo_url: book["volumeInfo"]["imageLinks"]["thumbnail"],
+#     year: book["volumeInfo"]['publishedDate'],
+#     pages_number: book["volumeInfo"]['pageCount']
+#   )
+#   book.save
+#   puts "creating book #{book.id}, #{book.title}"
+# end
+# puts "done"
+
+
+
+
+
+require "podcast_api"
+
+api_key = ENV["LISTEN_API_KEY"]
+client = PodcastApi::Client.new(api_key: api_key)
+
+puts "Creating Podcasts"
+
+response = client.search(q: 'startup', type: 'podcasts')
+puts JSON.parse(response.body).results
+
+# podcasts[:results].each do |podcast|
+#   podcast = Podcast.new(
+#     title: podcasts[:title_original],
+#     publisher: podcasts[:podcast][:publisher_original],
+#     description: podcasts[:description_original],
+#     image_url: podcasts[:podcast][:image],
+#     lenght: podcasts[:audio_lenght_sec]
+#   )
+#   podcast.save
+#   puts "creating book #{podcast.id}, #{podcast.title}"
+# end
+# puts "done"
