93ba76f1132a0b97cf593f77cc079dbaa5d77578
Thomas Mattelaer mattelaer.thomas@hotmail.com <mattelaer.thomas@hotmail.com>
2023-03-09 21:15:46 +0100

    alert list_books when you try to add a book in your list

--- app/controllers/list_books_controller.rb
+++ app/controllers/list_books_controller.rb
@@ -4,8 +4,13 @@ class ListBooksController < ApplicationController
     @book = Book.find(params[:list_book][:book_id])
     @list_book.book = @book
     if @list_book.save
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "Added to #{@list_book.list.name}"
+    elsif @list_book.list.nil?
+      redirect_to book_path(@book), alert: "You need to select a list"
+    elsif @list_book.list.books.include?(@book)
+      redirect_to book_path(@book), alert: "This book is already in this list"
     else
+      # render "movies/show", status: :unprocessable_entity
     end
   end
 
