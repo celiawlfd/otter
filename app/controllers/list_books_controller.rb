714b40e6d80d7b9c84895d4dcebf2db8cdd63ef9
maud bouyala <maud.bouyala@yahoo.fr>
2023-03-09 11:15:15 +0100

    added the list-books-controller

---
+++ app/controllers/list_books_controller.rb
@@ -0,0 +1,21 @@
+class ListBooksController < ApplicationController
+  def create
+    @list_book = ListBook.new(list_book_params)
+    @book = Book.find(params[:list_book][:book_id])
+    @list_book.book = @book
+    @list_book.save
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @list_book = ListBook.find(params[:id])
+    @list_book.destroy
+    redirect_to list_path(@list_book.list), status: :see_other
+  end
+
+  private
+
+  def list_book_params
+    params.require(:list_book).permit(:comment, :list_id)
+  end
+end
