5db039584136a4542b3e6450cd1c4ef2197b8f52
maud bouyala <maud.bouyala@yahoo.fr>
2023-03-10 14:48:43 +0100

    added the reviews to podcasts and finished the show

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -6,10 +6,10 @@ class PodcastsController < ApplicationController
   def show
     @podcast = Podcast.find(params[:id])
 
-    # @myreview = PodcastReview.find_by(podcast: @podcast, user: current_user)
-    # @podcast_review = PodcastReview.new
-    # @reviews = @podcast.podcast_reviews
-    # @rating_av = rating_average(@reviews)
+    @myreview = PodcastReview.find_by(podcast: @podcast, user: current_user)
+    @podcast_review = PodcastReview.new
+    @reviews = @podcast.podcast_reviews
+    @rating_av = rating_average(@reviews)
 
     @list = List.last
     @list_podcast = ListPodcast.new
@@ -21,12 +21,12 @@ class PodcastsController < ApplicationController
     params.require(:podcast).permit(:title, :description, :photo_url, :length, :publisher, :link)
   end
 
-  # def rating_average(reviews)
-  #   ratings = []
-  #   reviews.each do |review|
-  #     ratings << review.rating
-  #   end
+  def rating_average(reviews)
+    ratings = []
+    reviews.each do |review|
+      ratings << review.rating
+    end
 
-  #   ratings.sum.fdiv(ratings.size).round(2)
-  # end
+    ratings.sum.fdiv(ratings.size).round(2)
+  end
 end
