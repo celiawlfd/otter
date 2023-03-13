29f3f0d5efa2aaeb8414d2459ce55db35c2e68a3
Thomas Mattelaer <120176297+ThomasMattelaer@users.noreply.github.com>
2023-03-13 19:13:44 +0100

    Update movie_controller.js

--- app/javascript/controllers/movie_controller.js
+++ app/javascript/controllers/movie_controller.js
@@ -11,23 +11,23 @@ static targets = ["input", "results"]
 
   //MOVIES
 
-  // fetchMovies(query) {
-  //   fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
-  //   //console.log(response)
-  //     .then(response => response.json())
-  //     .then(data => this.insertMovies(data))
-  // }
-  // insertMovies(data) {
-  //   data.Search.forEach((result) => {
-  //     console.log(result);
-  //     const movieTag =`<div class="feed-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')">
-  //     <div>
-  //       <p class="card-titre">${result.Title} </p>
-  //     </div>
-  //   </div>`
-  //     this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
-  //   })
-  // }
+   fetchMovies(query) {
+     fetch(`http://www.omdbapi.com/?s=${query}&apikey=adf1f2d7`)
+     //console.log(response)
+       .then(response => response.json())
+       .then(data => this.insertMovies(data))
+   }
+   insertMovies(data) {
+     data.Search.forEach((result) => {
+       console.log(result);
+      const movieTag =`<div class="feed-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')">
+       <div>
+         <p class="card-titre">${result.Title} </p>
+       </div>
+     </div>`
+       this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
+     })
+   }
 
 
   //BOOKS
@@ -57,7 +57,7 @@ static targets = ["input", "results"]
     console.log(this.event)
     event.preventDefault()
     this.resultsTarget.innerHTML = ""
-    // this.fetchMovies(this.inputTarget.value)
+    this.fetchMovies(this.inputTarget.value)
     this.fetchBooks(this.inputTarget.value)
   }
 }
