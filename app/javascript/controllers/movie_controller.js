51ec77214710f7381a5fc6af4f5a3a300711570c
Thomas Mattelaer mattelaer.thomas@hotmail.com <mattelaer.thomas@hotmail.com>
2023-03-15 10:30:56 +0100

    start of the branch

--- app/javascript/controllers/movie_controller.js
+++ app/javascript/controllers/movie_controller.js
@@ -17,25 +17,25 @@ static targets = ["input", "results"]
     data.Search.forEach((result) => {
     const movieTag = `
 
-    <form class="simple_form new_movie" id="new_movie" novalidate="novalidate" action="/movies" accept-charset="UTF-8" method="post"><input type="hidden" name="authenticity_token" value="b9brpM_3fEkQR9iRK2tkBKsOslAu0LrhAY8VYAnlFB5XjAlI_kujodjFrwnkrgIaRsHk8iveDfe8zQK3V2pu9Q" autocomplete="off" />
-      <div class="mb-3 movie_title"><input class="form-control" type="hidden" name="movie[title]" id="movie_title" value="${result.Title}"/></div>
-      <div class="mb-3 movie_description"><textarea class="form-control" style="display:none;" name="movie[description]" id="movie_description" >look at Imdb to have more information.</textarea></div>
-      <div class="mb-3 movie_year"><input class="form-control" type="hidden" step="1" name="movie[year]" id="movie_year" value="${result.Year}"/></div>
-      <div class="mb-3 movie_photo_url"><input class="form-control" type="hidden" name="movie[photo_url]" id="movie_photo_url" value="${result.Poster}"/></div>
-      <input type="submit" name="commit" value="${result.Title}" data-disable-with="Create Movie" class ="culturale-card"style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')"/>
-    </form>
+<form class="simple_form new_movie" id="new_movie" novalidate="novalidate" action="/movies" accept-charset="UTF-8" method="post"><input type="hidden" name="authenticity_token" value="b9brpM_3fEkQR9iRK2tkBKsOslAu0LrhAY8VYAnlFB5XjAlI_kujodjFrwnkrgIaRsHk8iveDfe8zQK3V2pu9Q" autocomplete="off" />
+  <div class="mb-3 movie_title"><input class="form-control" type="hidden" name="movie[title]" id="movie_title" value="${result.Title}"/></div>
+  <div class="mb-3 movie_description"><textarea class="form-control" style="display:none;" name="movie[description]" id="movie_description" >look at Imdb to have more information.</textarea></div>
+  <div class="mb-3 movie_year"><input class="form-control" type="hidden" step="1" name="movie[year]" id="movie_year" value="${result.Year}"/></div>
+  <div class="mb-3 movie_photo_url"><input class="form-control" type="hidden" name="movie[photo_url]" id="movie_photo_url" value="${result.Poster}"/></div>
+  <input type="submit" name="commit" value="${result.Title}" data-disable-with="Create Movie" class ="culturale-card"style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')"/>
+</form>
 
 
 
 
-    <div class="culturale-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')">
-    <div>
-      <div class="d-flex align-items-center card-stickers">
-        <small class="card-categorie" style="background-color: rgba(255, 187, 43, 0.8); border: 1px solid rgb(255, 187, 43)">Movie</small>
-      </div>
-      <p class="card-titre"> ${result.Title}</p>
-      </div>
-    </div>`
+       <div class="culturale-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.Poster}, crop: :fill')">
+       <div>
+         <div class="d-flex align-items-center card-stickers">
+           <small class="card-categorie" style="background-color: rgba(255, 187, 43, 0.8); border: 1px solid rgb(255, 187, 43)">Movie</small>
+         </div>
+         <p class="card-titre"> ${result.Title}</p>
+         </div>
+       </div>`
 
        this.resultsTarget.insertAdjacentHTML("beforeend", movieTag)
      })
@@ -54,27 +54,33 @@ static targets = ["input", "results"]
   insertBooks(data) {
     data["items"].forEach((result) => {
 
-      const bookTag = `
-      <form class="simple_form new_book" id="new_book" novalidate="novalidate" action="/books" accept-charset="UTF-8" method="post"><input type="hidden" name="authenticity_token" value="b9brpM_3fEkQR9iRK2tkBKsOslAu0LrhAY8VYAnlFB5XjAlI_kujodjFrwnkrgIaRsHk8iveDfe8zQK3V2pu9Q" autocomplete="off" />
-        <div class="mb-3 book_title"><input class="form-control" type="hidden" name="book[title]" id="book_title" value="${result.volumeInfo.title}"/></div>
-        <div class="mb-3 book_description"><textarea class="form-control" style="display:none;" name="book[description]" id="book_description" >${result.volumeInfo.description}</textarea></div>
-        <div class="mb-3 book_author"><input class="form-control" type="hidden" name="book[author]" id="book_author" value="${result.volumeInfo.authors[0]}"/></div>
-        <div class="mb-3 book_pages_number"><input class="form-control" type="hidden" name="book[pages_number]" id="book_pages_number" value="${result.volumeInfo.pageCount}"/></div>
-        <div class="mb-3 book_year"><input class="form-control" type="hidden" step="1" name="book[year]" id="book_year" value="${result.volumeInfo.publishedDate}"/></div>
-        <div class="mb-3 book_photo_url"><input class="form-control" type="hidden" name="book[photo_url]" id="book_photo_url" value="${result.volumeInfo.imageLinks.thumbnail}"/></div>
-        <div class="text-center">
-          <input type="submit" name="commit" value="${result.volumeInfo.title}" data-disable-with="Create Movie" class ="culturale-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.volumeInfo.imageLinks.thumbnail}, crop: :fill')" />
-        </div>
-      </form>
+      console.log(result.volumeInfo.title)
+    const bookTag = `
+    <form class="simple_form new_book" id="new_book" novalidate="novalidate" action="/books" accept-charset="UTF-8" method="post"><input type="hidden" name="authenticity_token" value="b9brpM_3fEkQR9iRK2tkBKsOslAu0LrhAY8VYAnlFB5XjAlI_kujodjFrwnkrgIaRsHk8iveDfe8zQK3V2pu9Q" autocomplete="off" />
+      <div class="mb-3 book_title"><input class="form-control" type="hidden" name="book[title]" id="book_title" value="${result.volumeInfo.title}"/></div>
+      <div class="mb-3 book_description"><textarea class="form-control" style="display:none;" name="book[description]" id="book_description" >${result.volumeInfo.description}</textarea></div>
+      <div class="mb-3 book_author"><input class="form-control" type="hidden" name="book[author]" id="book_author" value="${result.volumeInfo.authors[0]}"/></div>
+      <div class="mb-3 book_pages_number"><input class="form-control" type="hidden" name="book[pages_number]" id="book_pages_number" value="${result.volumeInfo.pageCount}"/></div>
+      <div class="mb-3 book_year"><input class="form-control" type="hidden" step="1" name="book[year]" id="book_year" value="${result.volumeInfo.publishedDate}"/></div>
+      <div class="mb-3 book_photo_url"><input class="form-control" type="hidden" name="book[photo_url]" id="book_photo_url" value="${result.volumeInfo.imageLinks.thumbnail}"/></div>
+      <input type="submit" name="commit" value="${result.volumeInfo.title}" data-disable-with="Create Movie" class ="culturale-card"style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.volumeInfo.imageLinks.thumbnail}, crop: :fill')" />
+    </form>
 
-    <div class="culturale-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.volumeInfo.imageLinks.thumbnail}, crop: :fill')">
+
+
+
+
+
+
+
+    <div class="culturale-card" style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url('${result.volumeInfo.imageLinks.thumbnail}, crop: :fill %>')">
       <div>
         <div class="d-flex align-items-center card-stickers">
           <small class="card-categorie" style="background-color: rgba(205, 76, 236, 0.8); border: 1px solid rgb(205, 76, 236)">Book</small>
         </div>
         <p class="card-titre"> ${result.volumeInfo.title}</p>
-      </div>
-    </div>`
+        </div>
+      </div>`
 
       this.resultsTarget.insertAdjacentHTML("beforeend", bookTag)
     })
@@ -87,7 +93,7 @@ static targets = ["input", "results"]
       headers : {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization' : 'Bearer BQC1GfNfLbcoqW7U-q_uT2_EcMAqdyKX3g-pSimyyqSV5RwgmG5XJ_E37BXcHhoa5Sbd3mJ8JPUn_p9hOwLAKRdGzTX2pvfgQDgVi4CLcTnAJ_qQIN56zro3_QYASCNdugLWbs6HXjNqhXkcweupmjhsfp4NEnLxCaySn8yz6dVw'
+        'Authorization' : 'Bearer BQB8Q5pRi8VIQJaZKHlSOdVXMs52Bms3_gKgUELIviTyCjVWAeDBY1iUGPNeZRhFK5KAQJr51WQpGTHKDSSTYdQ9TyPInEVZF1Q6L2IrlfqW8gYnRRJlxt8axuMO1FbKMck4NDrXgUprmSt2YNg-lYtuGqJ2S9psEmvxUk7unSM1'
       }
     })
       .then(response => response.json())
@@ -106,7 +112,7 @@ static targets = ["input", "results"]
     <div class="mb-3 podcast_publisher"><input class="form-control" type="hidden" step="1" name="podcast[publisher]" id="podcast_publisher" value="${result.publisher}"/></div>
     <div class="mb-3 podcast_photo_url"><input class="form-control" type="hidden" name="podcast[photo_url]" id="podcast_photo_url" value="${result["images"][0]["url"]}"/></div>
     <input type="submit" name="commit" value="${result.name}" data-disable-with="Create Movie" class ="culturale-card"style="width: 100%, height: 100%; background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.8)), url(${result["images"][0]["url"]})" />
-    </form>
+  </form>
 
 
 
@@ -122,7 +128,7 @@ static targets = ["input", "results"]
         </div>
         <p class="card-titre"> ${result.name}</p>
         </div>
-    </div>`
+      </div>`
       this.resultsTarget.insertAdjacentHTML("beforeend", podcastTag)
     })
   }
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,9 +7,11 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true
   validates :description, presence: true
 
+
   include PgSearch::Model
   multisearchable against: [:title, :description]
-
-
-
+  pg_search_scope :search_by_title, against: :title
+  # using: {
+  #   tsearch: { prefix: true }
+  # }
 end
