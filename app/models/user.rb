bd39b0b3b58821cf17adbeffb27ecd09218893ac
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-03-16 14:49:05 +0100

    error in validation photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one_attached :photo, prescence: true
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
